8737b9fabedc89704bee91352e1ed703e4dbcd34
Cristian Villafane <cristianv129@hotmail.com>
2020-10-26 16:03:15 -0300

    started structured defined

---
+++ src/scripts/index.js
@@ -0,0 +1,24 @@
+let navButton = document.getElementsByClassName("menu__button")[0];
+let menu__list = document.getElementsByClassName("menu__list")[0];
+
+navButton.addEventListener('click', () => {
+    if(window.innerWidth < 800) {
+        menu__list.classList.toggle('menu__list--hidden');
+        menu__list.classList.toggle('menu__list--showed');
+    }
+})
+
+window.onload = () => {
+    setHeight();
+    // ocultar o mostrar listas
+}
+
+window.onresize = () => {
+    // ocultar o mostrar liistas
+}
+
+function setHeight(){
+    let itemsQuantity = menu__list.children.length;
+    let heightNumber = itemsQuantity * 2.34;
+    document.documentElement.style.setProperty("--menu__list-height", heightNumber + 'em');
+}
